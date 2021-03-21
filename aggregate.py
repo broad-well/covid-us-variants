6da84c305cb693e862876e26aba31a231501abee
Michael Peng <michael@mcmoo.org>
2021-03-21 19:14:42 -0400

    Fix 01-31 to 03-02 update processing

--- aggregate.py
+++ aggregate.py
@@ -12,9 +12,9 @@ variant_names = {
 
 
 def read_rows(_date: date) -> pd.DataFrame:
-    df: pd.DataFrame = pd.read_csv(f'updates/{_date.isoformat()}.csv').dropna()
+    df: pd.DataFrame = pd.read_csv(f'updates/{_date.isoformat()}.csv')
     banned_states = ['Total']
-    df = df[~df.State.isin(banned_states)]
+    df = df[~df.State.isin(banned_states)][~df.State.isna()]
     df['State'] = df['State'].replace('District of Columbia', 'DC')
     cols = df.columns.values.tolist()
     if 'Rate' in cols:
@@ -28,9 +28,7 @@ def read_rows(_date: date) -> pd.DataFrame:
         df['b1351'] = 0
         return df
     elif 'filter' in cols:
-        return df[['State'] + [in_col for _, in_col in variant_names.values()]] \
-            .drop_duplicates(subset=['State']) \
-            .rename(columns=dict([(in_col, code) for code, (_, in_col) in variant_names.items()] + [('State', 'state')]))
+        return df[['State'] + [in_col for (_, in_col) in variant_names.values()]].drop_duplicates(subset=['State']).rename(columns=dict([(in_col, code) for code, (_, in_col) in variant_names.items()] + [('State', 'state')])).fillna(0)
     elif 'B.1.1.7 Variant ' in cols:
         return df.rename(columns=dict([(in_col, code) for code, (_, in_col) in variant_names.items()] + [('State', 'state')]))
 
@@ -44,13 +42,12 @@ if __name__ == '__main__':
                 print(f'warning: {_date.date().isoformat()}')
                 continue
             df['date'] = _date.date()
-            print(df)
             gdf = gdf.append(df)
         except FileNotFoundError:
             print(f'omitted {_date.date().isoformat()}')
 
 
     for col in variant_names.keys():
-        gdf[col] = gdf[col].astype('int32')
+        gdf[col] = gdf[col].fillna(0).astype('int32')
 
-    gdf.to_csv('us-states.csv', index=False)
+    gdf.sort_values(by=['date', 'state']).to_csv('us-states.csv', index=False)
